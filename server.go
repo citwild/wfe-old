05b70be91dd6be426f0f33608931ccdee7edabd5
Hasit Mistry <hasit@uw.edu>
2016-04-23 12:40:54 -0700

    Read template form data and print on console

--- server.go
+++ server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/gin-gonic/gin"
 )
 
+// Login is a struct to get form data from website
 type Login struct {
 	Email    string `form:"email" json:"email" binding:"required"`
 	Password string `form:"password" json:"password" binding:"required"`
@@ -17,15 +21,33 @@ func main() {
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Wide-Field Ethnography",
-		})
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 	})
 
 	router.POST("/auth", func(c *gin.Context) {
 		var form Login
 		if c.Bind(&form) == nil {
-			log.Printf("Email: %s\t Password: %s\n", form.Email, form.Password)
+			dbInstance := dynamodb.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
+			params := &dynamodb.GetItemInput{
+				Key: map[string]*dynamodb.AttributeValue{ // Required
+					"Email": { // Required
+						S: aws.String(form.Email),
+					},
+				},
+				TableName:      aws.String("WFE"),
+				ConsistentRead: aws.Bool(true),
+			}
+			resp, err := dbInstance.GetItem(params)
+			if err != nil {
+				c.JSON(401, gin.H{"status": "DB error"})
+			} else {
+				fmt.Println(*resp.Item["Password"].S)
+				// if *resp.Item["Email"].S == form.Email && *resp.Item["Password"].S == form.Password {
+				// 	c.JSON(200, gin.H{"status": "Logged in!"})
+				// } else {
+				// 	c.JSON(401, gin.H{"status": "Unauthorized"})
+				// }
+			}
 		}
 	})
 
