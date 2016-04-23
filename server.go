f117f80c17a01ffb55ec245c4c99175fd4227ad1
Hasit Mistry <hasit@uw.edu>
2016-04-23 14:11:49 -0700

    Fetch list of datasets associated with user from the DB

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,36 +21,57 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
-	})
-
-	router.POST("/auth", func(c *gin.Context) {
-		var form Login
-		if c.Bind(&form) == nil {
-			dbInstance := dynamodb.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
-			params := &dynamodb.GetItemInput{
-				Key: map[string]*dynamodb.AttributeValue{ // Required
-					"Email": { // Required
-						S: aws.String(form.Email),
-					},
-				},
-				TableName:      aws.String("WFE"),
-				ConsistentRead: aws.Bool(true),
-			}
-			resp, err := dbInstance.GetItem(params)
-			if err != nil {
-				c.JSON(401, gin.H{"status": "DB error"})
-			} else {
-				fmt.Println(*resp.Item["Password"].S)
-				// if *resp.Item["Email"].S == form.Email && *resp.Item["Password"].S == form.Password {
-				// 	c.JSON(200, gin.H{"status": "Logged in!"})
-				// } else {
-				// 	c.JSON(401, gin.H{"status": "Unauthorized"})
-				// }
-			}
-		}
-	})
+	router.GET("/", wfeIndex)
+	router.POST("/auth", userAuth)
 
 	router.Run(":8080")
+}
+
+func wfeIndex(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+}
+
+func userAuth(c *gin.Context) {
+	var form Login
+	if c.Bind(&form) == nil {
+		dbInstance := dynamodb.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
+		params := &dynamodb.GetItemInput{
+			Key: map[string]*dynamodb.AttributeValue{ // Required
+				"Email": { // Required
+					S: aws.String(form.Email),
+				},
+			},
+			TableName:      aws.String("WFE"),
+			ConsistentRead: aws.Bool(true),
+		}
+		resp, err := dbInstance.GetItem(params)
+		if err != nil {
+			c.JSON(401, gin.H{"status": "DB error"})
+		} else {
+			if *resp.Item["Email"].S == form.Email && *resp.Item["Password"].S == form.Password {
+				// c.JSON(200, gin.H{"status": "Logged in!"})
+				for _, dataset := range resp.Item["Datasets"].SS {
+					s3instance := s3.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
+					page := 0
+					err := s3instance.ListObjectsPages(&s3.ListObjectsInput{
+						Bucket: dataset,
+					}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
+						fmt.Println("Page,", page)
+						for _, obj := range p.Contents {
+							fmt.Println("Object:", *obj.Key)
+						}
+						return true
+					})
+					if err != nil {
+						fmt.Println("failted to list objects", err)
+					}
+				}
+			} else {
+				// c.JSON(401, gin.H{"status": "Unauthorized"})
+				c.HTML(http.StatusUnauthorized, "index.tmpl", gin.H{
+					"message": "Invalid login information.",
+				})
+			}
+		}
+	}
 }
