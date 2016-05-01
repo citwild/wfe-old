db1c5d202ccc7e83e5c01f2dca80122cff54af77
Miles Dowe <mdowe@hollandamerica.com>
2016-05-01 10:53:37 -0700

    Add logger objects to server

--- server.go
+++ server.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"net/http"
 
+    "log"
+    "io"
+    "io/ioutil"
+    "os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
@@ -22,8 +27,36 @@ type SelectedBucket struct {
 	Bucket string `form:"selectbucket" json:"selectbucket" binding:"required"`
 }
 
+var (
+    Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
+func Init(
+    traceHandle io.Writer,
+    infoHandle io.Writer,
+    warningHandle io.Writer,
+    errorHandle io.Writer) {
+
+    Trace = log.New(traceHandle,
+        "TRACE: ",
+        log.Ldate|log.Ltime|log.Lshortfile);
+    Info = log.New(infoHandle,
+        "INFO: ",
+        log.Ldate|log.Ltime|log.Lshortfile);
+    Warning = log.New(warningHandle,
+        "WARNING: ",
+        log.Ldate|log.Ltime|log.Lshortfile);
+    Error = log.New(errorHandle,
+        "ERROR: ",
+        log.Ldate|log.Ltime|log.Lshortfile);
+}
+
 // TODO: requestccess route
 func main() {
+    Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr);
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.tmpl")
 	// Uncomment the next line when ready for release.
